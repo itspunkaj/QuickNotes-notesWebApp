066ad43de5f1d99f7a5fd95bbeef9fe755e29edd
itspunkaj <95837801+itspunkaj@users.noreply.github.com>
2023-06-04 11:52:45 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Icon from "@mui/icons-material/TextSnippetOutlined";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <Icon />
+        QuickNotes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
